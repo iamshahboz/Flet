fbf83bab5acfa966b74417a43a9d7c45979d3682
Shahboz <sharafzodashahboz@gmail.com>
2025-05-03 03:19:02 +0500

    To Do app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
-import  flet as ft 
+import flet as ft
+
+class TodoApp(ft.Column):
+
+    def __init__(self):
+        super().__init__()
+        self.new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+        self.tasks_view = ft.Column()
+        self.width=600
+        self.controls = [
+            ft.Row(
+                controls=[
+                    self.new_task,
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD, on_click=self.add_clicked
+                    ),
+                ],
+            ),
+            self.tasks_view,
+        ]
+    def add_clicked(self, e):
+        self.tasks_view.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
 
 def main(page: ft.Page):
-    t = ft.Text(value='Hello world', color='green')
-    page.controls.append(t)
+    page.title = "To - Do App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.update()
 
+    todo = TodoApp()
+
+    page.add(todo)
 
 ft.app(main)
