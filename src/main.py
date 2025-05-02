722058372f5c76ea211f9a96eb691317693a3e0c
Shahboz <sharafzodashahboz@gmail.com>
2025-05-03 03:52:42 +0500

    flet To Do app created

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 import flet as ft
 
 class TodoApp(ft.Column):
-
     def __init__(self):
         super().__init__()
         self.new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
-        self.tasks_view = ft.Column()
-        self.width=600
+        self.tasks = ft.Column()
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[
+                ft.Tab(text="all"),
+                ft.Tab(text="active"),
+                ft.Tab(text="completed"),
+            ],
+        )
+
+        self.width = 600
         self.controls = [
+            self.filter,
             ft.Row(
                 controls=[
                     self.new_task,
@@ -16,21 +26,120 @@ class TodoApp(ft.Column):
                     ),
                 ],
             ),
-            self.tasks_view,
+            self.tasks,
         ]
+
     def add_clicked(self, e):
-        self.tasks_view.controls.append(ft.Checkbox(label=self.new_task.value))
+        if self.new_task.value.strip() == "":
+            return
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+        self.tasks.controls.append(task)
         self.new_task.value = ""
         self.update()
 
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
+    def before_update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and not task.completed)
+                or (status == "completed" and task.completed)
+            )
+
+    def tabs_changed(self, e):
+        self.update()
+
+    def task_status_change(self):
+        self.update()
+
+    def update(self):
+        self.before_update()
+        super().update()
+
+
+class Task(ft.Column):
+    def __init__(self, task_name, task_status_change, task_delete):
+        super().__init__()
+        self.completed = False
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+
+        self.display_task = ft.Checkbox(
+            value=False, label=task_name, on_change=self.status_changed
+        )
+
+        self.edit_name = ft.TextField(expand=1)
+
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_task,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.Icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            ft.Icons.DELETE_OUTLINE,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.Colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+
+        self.controls = [self.display_view, self.edit_view]
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def delete_clicked(self, e):
+        self.task_delete(self)
+
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change()
+
 
 def main(page: ft.Page):
-    page.title = "To - Do App"
+    page.title = "To-Do App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.update()
 
     todo = TodoApp()
-
     page.add(todo)
 
 ft.app(main)
